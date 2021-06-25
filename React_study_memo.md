cb9bbdd3acc95c2f006bb6f4470c3fe83192c2c6
toru <t02f447f@gmail.com>
2021-06-26 06:52:23 +0900

    create class component and functional component

--- React_study_memo.md
+++ React_study_memo.md
@@ -26,3 +26,13 @@ yarn run start
 
 - return dom の dom 内は１つのタグという制約があり、不要な div タグなどが発生するが、
   <React.Fragment>タグで囲むとそれが不要になる
+
+## トランスパイラー
+
+- React の場合、jsx を babel がトランスパイルしている
+
+## Function Component と Class component
+
+- 違いは Class Component では state、ライフサイクルが使え、function component ではそれらが使えない
+- しかし現在は function component が推奨されている
+- 理由：React Hooks 導入により function component でも usesState、useEffect などを使い、ライフサイクルに相当するものが使えるようになってきている。
--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
+// Classコンポーネント
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input
+//           type="text"
+//           id="bar"
+//           onChange={() => {
+//             console.log('I am Changed!');
+//           }}
+//         />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// Functionコンポーネント
 function App() {
-  //   const greeting = 'Hi!!!!';
   const dom = (
     <React.Fragment>
       <label htmlFor="bar">bar</label>
@@ -13,9 +31,17 @@ function App() {
           console.log('I am Clicked!');
         }}
       />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
     </React.Fragment>
   );
   return dom;
 }
+
+const Cat = () => {
+  return <div>Meow!</div>;
+};
 
 export default App;
