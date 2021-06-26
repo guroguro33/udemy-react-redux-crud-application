efba026f55562e748cf82cb7796c9989b3d49c33
toru <t02f447f@gmail.com>
2021-06-26 21:59:04 +0900

    props

--- React_study_memo.md
+++ React_study_memo.md
@@ -36,3 +36,50 @@ yarn run start
 - 違いは Class Component では state、ライフサイクルが使え、function component ではそれらが使えない
 - しかし現在は function component が推奨されている
 - 理由：React Hooks 導入により function component でも usesState、useEffect などを使い、ライフサイクルに相当するものが使えるようになってきている。
+
+## props
+
+- 親コンポーネントから子コンポーネントに変数を渡す
+- User.defaultProps で、デフォルト値を指定できる
+
+```javascript
+// Functionコンポーネント
+function App() {
+  const profiles = [
+    {
+      name: 'Taro',
+      age: 10,
+    },
+    {
+      name: 'Hanako',
+      age: 5,
+    },
+    {
+      name: 'Noname', // ageがないが、defaultPropsで指定
+    },
+  ];
+  const dom = (
+    <React.Fragment>
+      {/* 親コンポーネントで変数propsの中身 profile, indexを渡す */}
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
+    </React.Fragment>
+  );
+  return dom;
+}
+
+// 子コンポーネント propsで親の変数を受け取る
+const User = (props) => {
+  return (
+    <div>
+      Hi!, I am {props.name}, and {props.age} years old!
+    </div>
+  );
+};
+
+// 子コンポーネントのデフォルト値を設定する
+User.defaultProps = {
+  age: 1,
+};
+```
--- src/App.js
+++ src/App.js
@@ -1,47 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
 
-// Classコンポーネント
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input
-//           type="text"
-//           id="bar"
-//           onChange={() => {
-//             console.log('I am Changed!');
-//           }}
-//         />
-//       </React.Fragment>
-//     );
-//   }
-// }
-
 // Functionコンポーネント
 function App() {
+  const profiles = [
+    {
+      name: 'Taro',
+      age: 10,
+    },
+    {
+      name: 'Hanako',
+      age: 5,
+    },
+    {
+      name: 'Noname',
+    },
+  ];
   const dom = (
     <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input
-        type="text"
-        id="bar"
-        onChange={() => {
-          console.log('I am Clicked!');
-        }}
-      />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {/* 親コンポーネントで変数propsを渡す */}
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
     </React.Fragment>
   );
   return dom;
 }
 
-const Cat = () => {
-  return <div>Meow!</div>;
+// 子コンポーネント propsで親の変数を受け取る
+const User = (props) => {
+  return (
+    <div>
+      Hi!, I am {props.name}, and {props.age} years old!
+    </div>
+  );
+};
+
+// 子コンポーネントのデフォルト値を設定する
+User.defaultProps = {
+  age: 1,
 };
 
 export default App;
